00cf0ff623015f26dc71aef5216bc4080c03b451
Heyuan <shiheyuan>
2016-12-21 13:38:39 +0800

    add both packets

---
+++ src/jenetics/GTS.java
@@ -0,0 +1,15 @@
+package jenetics;
+
+import org.jenetics.IntegerChromosome;
+
+public class GTS {
+	public static void GTS_step() {
+		IntegerChromosome integerChromosome = new IntegerChromosome(0, 1);
+		System.out.println(integerChromosome);
+
+	}
+
+	public static void main(String[] args) {
+		GTS_step();
+	} c
+}
---
+++ src/jenetics/Individual.java
@@ -0,0 +1,13 @@
+package jenetics;
+
+import org.jenetics.IntegerChromosome;
+
+public class Individual {
+	public IntegerChromosome integerChromosome;
+
+	public int val;
+
+	public Individual(int len) {
+		integerChromosome = new IntegerChromosome(0, 10, len);
+	}
+}
